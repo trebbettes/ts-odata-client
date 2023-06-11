c88db7ae59b5746ba1705447b2da1a2f85266d43
Tom Rebbettes <trebbettes@gmail.com>
2023-06-11 23:02:37 +0000

    Introduced GetResult and GetManyResult classes

--- src/lib/ODataResponse.ts
+++ src/lib/ODataResponse.ts
@@ -11,7 +11,7 @@ export interface ODataResponse {
 /**
  * Represents an OData response with 0 or more results.
  */
-export interface ODataQueryResponse<T> extends ODataResponse {
+export interface ODataQueryResponse<U> extends ODataResponse {
     /**
      * If server-side paging is implemented, this will hold the complete URL of the query used to retrieve the next 'page' of results.
      */
@@ -20,13 +20,40 @@ export interface ODataQueryResponse<T> extends ODataResponse {
     /**
      * The results of the OData query
      */
-    value: T[];
+    value: U[];
 }
 
 /**
  * Represnets an OData response with 0 or more results that should also include the total number of items in the results.
  * This value will be different than the number of items in @member {value} if server-side paging is implemented.
  */
-export interface ODataQueryResponseWithCount<T> extends ODataQueryResponse<T> {
+export interface ODataQueryResponseWithCount<U> extends ODataQueryResponse<U> {
     ["@odata.count"]?: number;
+}
+
+export class GetResult<T> {
+
+    constructor(response: ODataResponse) {
+        this.response = response;
+    }
+
+    response: ODataResponse;
+
+    getValue(): T {
+        return this.response as unknown as T;
+    }
+
+}
+
+export class GetManyResult<T, U> {
+    
+    constructor(response: ODataQueryResponse<U>) {
+        this.response = response;
+    }
+    
+    response: ODataQueryResponse<U>;
+
+    getValues(): T[] {
+        return this.response.value as unknown as T[];
+    }
 }
